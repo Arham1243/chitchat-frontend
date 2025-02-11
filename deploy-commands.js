7f0459b31794386936fa2e6be2b790b6eb41262d
Arham1243 <arham404khan@gmail.com>
2025-02-12 01:14:11 +0500

    add deploy file

---
+++ deploy-commands.js
@@ -0,0 +1,19 @@
+// touch .env
+// nano .env
+// apt update
+// apt install -y nodejs npm
+
+// curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+// apt install -y nodejs
+
+// npm install
+
+// npm run build
+
+// nvm install --lts
+// nvm use --lts
+// npm cache clean --force
+
+// npm install -g serve
+
+// serve -s dist
