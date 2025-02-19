c8dc4b4f31c1440de37f7dd0480589b56236bfd7
itsdanielbruh <danielkh.work@gmail.com>
2025-02-19 21:11:28 +0500

    add github links

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -7,9 +7,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div
-        class="w-screen h-screen flex justify-content-center align-items-center"
-    >
+    <div class="w-screen auth-main flex justify-content-center align-items-center">
         <div class="grid w-full justify-content-center">
             <div class="auth-wrapper">
                 <div class="text-center mb-4">
@@ -21,11 +19,26 @@ onMounted(() => {
             </div>
         </div>
     </div>
+    <div class="px-3 py-4 auth-footer">
+        <div class="flex gap-3 justify-content-center">
+            <a href="https://github.com/Arham1243/chitchat-frontend" target="_blank" rel="noopener noreferrer"
+                v-tooltip.bottom="`ChitChat Frontend`">
+                <Button icon="icon pi pi-github" variant="outlined" rounded />
+            </a>
+            <a href="https://github.com/Arham1243/chitchat-backend" target="_blank" rel="noopener noreferrer"
+                v-tooltip.bottom="`ChitChat Backend`">
+                <Button icon="icon pi pi-github" variant="outlined" rounded />
+            </a>
+        </div>
+    </div>
     <Toast />
 </template>
 
 <style scoped>
 .auth-wrapper {
     width: 303px;
+}
+.auth-main{
+    min-height: 93vh;
 }
 </style>
